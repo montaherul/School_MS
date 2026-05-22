3e75dd24d51b65bb90b81b81562ac57a7859f045
montaherul <c233210@ugrad.iiuc.ac.bd>
2026-05-23 03:28:26 +0600

    updates

---
+++ Migrations/20260522201352_SeedLeaveTypes2.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace SchoolManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedLeaveTypes2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "LeaveTypes",
+                columns: new[] { "Id", "IsActive", "IsPaid", "MaxDays", "Name" },
+                values: new object[,]
+                {
+                    { 1, true, true, 14, "Sick Leave" },
+                    { 2, true, true, 10, "Casual Leave" },
+                    { 3, true, true, 180, "Maternity Leave" },
+                    { 4, true, true, 15, "Paternity Leave" },
+                    { 5, true, false, 30, "Unpaid Leave" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "LeaveTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "LeaveTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "LeaveTypes",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "LeaveTypes",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "LeaveTypes",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
