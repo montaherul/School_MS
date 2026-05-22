0228d564afddaa7b6bdf141f52b92b8e93dee11a
Rakib <c233213@ugrad.iiuc.ac.bd>
2026-05-22 23:51:47 +0600

    principle pic done

--- obj/Debug/net8.0/SchoolManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/SchoolManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d4eec7b9e2a1d8d40b7ade32e6623df7b5fc194")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe2e2add823b948ba4aa7b3a382cfcb72991e7f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
