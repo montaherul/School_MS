e1b3d4cf82dc3bf73a5623c8be638c3b239c0740
montaherul <c233210@ugrad.iiuc.ac.bd>
2026-05-22 23:55:14 +0600

    app pass chnage

--- obj/Debug/net8.0/SchoolManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/SchoolManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe2e2add823b948ba4aa7b3a382cfcb72991e7f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+579e8ccf4e3b69ecc305ea702c47b651e78e15dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
